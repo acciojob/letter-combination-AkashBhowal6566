94bcd2a50b2df4b31285ba94746df2d7ed0d98e4
Akash Bhowal <akash.aug10@gmail.com>
2023-01-30 23:09:13 +0530

    First Commit

--- script.js
+++ script.js
@@ -1,32 +1,5 @@
 function letterCombinations(input_digit) {
   //Complete the function
-	if (digit == null || digit.length === 0) return [];
-
-  const map = {
-    2: 'abc',
-    3: 'def',
-    4: 'ghi',
-    5: 'jkl',
-    6: 'mno',
-    7: 'pqrs',
-    8: 'tuv',
-    9: 'wxyz',
-  };
-
-  const res = [];
-  const go = (i, s) => {
-    if (i === digit.length) {
-      res.push(s);
-      return;
-    }
-
-    for (const c of map[digit[i]]) {
-      go(i + 1, s + c);
-    }
-  };
-
-  go(0, '');
-  return res;
 
 }
 
